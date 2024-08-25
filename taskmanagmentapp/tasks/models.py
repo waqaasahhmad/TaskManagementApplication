4a7e7dc745ec02eef469e306a62648cc0245afc5
pc <pc@192.168.1.9>
2024-08-25 10:52:57 +0500

    add Task Model

--- taskmanagmentapp/tasks/models.py
+++ taskmanagmentapp/tasks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = {
+        "high" : "High",
+        "medium" : "Medium",
+        "low" : "LOW", 
+    }
+    STATUS_CHOICES = {
+        "completed" : "Completed",
+        "inprogress" : "In Progress",
+        "notstarted" : "Not Started"
+    }
+
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=200,null=True,blank=True)
+    description = models.TextField(max_length=500,null=True,blank=True)
+    priority = models.CharField(max_length=100,choices=PRIORITY_CHOICES,null=True,blank=True)
+    status = models.CharField(max_length=100,choices=STATUS_CHOICES,null=True,blank=True)
+    due_date = models.DateField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+
